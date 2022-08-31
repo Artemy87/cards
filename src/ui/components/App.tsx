4b9164291f8702c89fe2076274d06d33231a5bc5
Ivan <paster-ivan@yandex.by>
2022-08-31 18:04:35 +0300

    add folders cards and packs-list

--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -4,16 +4,16 @@ import './App.css'
 
 import { Navigate, Route, Routes } from 'react-router-dom'
 
-import { Login } from './f1-auth/a1-login/Login'
-import { Register } from './f1-auth/a2-register/Register'
-import { Profile } from './f1-auth/a3-Profile/Profile'
-import { Error404 } from './f1-auth/a4-404/Error404'
-import PasswordRecovery from './f1-auth/a5-password-recovery/PasswordRecovery'
-import Password from './f1-auth/a6-password/Password'
+import { Login } from './main/auth/a1-login/Login'
+import { Register } from './main/auth/a2-register/Register'
+import { Profile } from './main/auth/a3-Profile/Profile'
 
 import { initializeApp } from 'bll/reducers/appReducer'
 import { useAppDispatch } from 'common/hooks/hook'
 import { Header } from 'ui/components/header/Header'
+import { Error404 } from 'ui/components/main/auth/a4-404/Error404'
+import PasswordRecovery from 'ui/components/main/auth/a5-password-recovery/PasswordRecovery'
+import Password from 'ui/components/main/auth/a6-password/Password'
 
 export function App() {
   const dispatch = useAppDispatch()
