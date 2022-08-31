18091289a3ec3f082f2f14bf2169cd6d3db0401f
Artemy <artemij87work@gmail.com>
2022-08-31 19:05:08 +0300

    upd main structure

--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -4,16 +4,16 @@ import './App.css'
 
 import { Navigate, Route, Routes } from 'react-router-dom'
 
-import { Login } from './main/auth/a1-login/Login'
-import { Register } from './main/auth/a2-register/Register'
-import { Profile } from './main/auth/a3-Profile/Profile'
+import { Login } from './main/auth/Login/Login'
+import { Profile } from './main/auth/Profile/Profile'
+import { Register } from './main/auth/Register/Register'
 
 import { initializeApp } from 'bll/reducers/appReducer'
 import { useAppDispatch } from 'common/hooks/hook'
 import { Header } from 'ui/components/header/Header'
-import { Error404 } from 'ui/components/main/auth/a4-404/Error404'
-import PasswordRecovery from 'ui/components/main/auth/a5-password-recovery/PasswordRecovery'
-import Password from 'ui/components/main/auth/a6-password/Password'
+import { Error404 } from 'ui/components/main/auth/404/Error404'
+import PasswordRecovery from 'ui/components/main/auth/Password-recovery/PasswordRecovery'
+import Password from 'ui/components/main/auth/Password/Password'
 
 export function App() {
   const dispatch = useAppDispatch()
