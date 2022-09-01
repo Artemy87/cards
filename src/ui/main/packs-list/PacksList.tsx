986a99ca1069fd3204a099800af8cdda893f480b
Ivan <paster-ivan@yandex.by>
2022-09-01 16:02:16 +0300

    Refactoring PackList

--- src/ui/main/packs-list/PacksList.tsx
+++ src/ui/main/packs-list/PacksList.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { Navigate } from 'react-router-dom'
 
+import style from './PacksList.module.css'
+
 import { useAppSelector } from 'common/hooks/useAppSelector'
 import { Search } from 'ui/main/packs-list/search/Search'
 import { TablePacks } from 'ui/main/packs-list/table-packs/TablePacks'
@@ -14,10 +16,10 @@ export const PacksList = () => {
   }
 
   return (
-    <div>
-      <div>
+    <div className={style.packsList}>
+      <div className={style.packsListHeader}>
         <div>Packs List</div>
-        <button>Add new pack</button>
+        <button className={style.button}>Add new pack</button>
       </div>
       <Search />
       <TablePacks />
